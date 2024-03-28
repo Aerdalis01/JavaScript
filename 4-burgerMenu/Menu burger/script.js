6bf47dbbcbec81eb0efe3454ecc4f7584772c03c
Aerdalis01 <caillonaudric@hotmail.fr>
2024-03-28 15:10:48 +0100

    Maj mis en place de l'index, du css et des animations de bases du menu burger

--- 4-burgerMenu/Menu burger/script.js
+++ 4-burgerMenu/Menu burger/script.js
@@ -1,1 +1,17 @@
 console.log("connecté!")
+
+//Je sélectionne
+
+const icone = document.querySelector('.navbar-mobile i');
+console.log(icone);
+
+const divModal = document.querySelector('.modal');
+console.log(divModal);
+
+icone.addEventListener('click', function(){
+  console.log("icône cliqué");
+  divModal.classList.toggle('change-modal');
+  icone.classList.toggle('fa-times');
+  icone.classList.toggle('fa-bars');
+
+});
